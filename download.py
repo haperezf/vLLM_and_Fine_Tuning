c18d388deb7123490289fb770de90192a876c54a
haperezf <hecperez@hotmail.com>
2025-06-02 12:12:37 -0500

    .

--- download.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
-    local_dir="./models/mistral",
-    local_dir_use_symlinks=False  # Avoids symlink issues on Windows
-)
